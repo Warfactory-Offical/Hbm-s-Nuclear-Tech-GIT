2bc419b4011381ecf90601d13cc87ae87062b1c5
MrNorwood <killallniggerswhitepower88@nuke.africa>
2025-01-15 23:23:28 +0100

    Porting material prefixes from HBM to GT style part 2

--- src/main/java/api/hbm/material/MaterialPrefix.java
+++ src/main/java/api/hbm/material/MaterialPrefix.java
@@ -19,36 +19,44 @@ public class MaterialPrefix {
 
     public static final long M = 3628800;
     static int idCounter;
-    public final static MaterialPrefix INGOT = new MaterialPrefix("ingot", M, HBMMaterialIconType.ingot, Predicates.alwaysTrue(), ntmMaterial -> ntmMaterial.hasFlag(INGOT), null);
+
+
+    public static final Predicate<NTMMaterial> hasOreProperty = mat -> mat.hasProperty(PropertyKey.ORE);
+    public static final Predicate<NTMMaterial> hasGemProperty = mat -> mat.hasProperty(PropertyKey.GEM);
+    public static final Predicate<NTMMaterial> hasPowderProperty = mat -> mat.hasProperty(PropertyKey.POWDER);
+    public static final Predicate<NTMMaterial> hasIngotProperty = mat -> mat.hasProperty(PropertyKey.INGOT);
+    public static final Predicate<NTMMaterial> hasBedrockOreProperty = mat -> mat.hasProperty(PropertyKey.BEDROCK_ORE);
+
+    public final static MaterialPrefix INGOT = new MaterialPrefix("ingot", M, HBMMaterialIconType.ingot, Predicates.alwaysTrue(), hasOreProperty, null);
     public static final MaterialPrefix NUGGET = new MaterialPrefix("nugget", M/9, HBMMaterialIconType.nugget, Predicates.alwaysTrue(), ntmMaterial -> ntmMaterial.hasFlag(NUGGET), null );
-    public static final MaterialPrefix QUANTUM = new MaterialPrefix("quantum", M/72, HBMMaterialIconType.quantum, Predicates.alwaysTrue(), null); // 1/72 of an ingot, allows the ingot to be divisible through 2, 4, 6, 8, 9, 12, 24 and 36 (idk why we need it)
     public static final MaterialPrefix FRAGMENT = new MaterialPrefix("fragment", M/9, HBMMaterialIconType.fragment, ntmMaterial -> ntmMaterial.hasFlag(FRAGMENT) && !ntmMaterial.hasFlag(NUGGET));
-    public static final MaterialPrefix DUSTTINY = new MaterialPrefix("powder_tiny", M/9, HBMMaterialIconType.dustTiny,  ntmMaterial -> ntmMaterial.hasFlag(DUST));
+    public static final MaterialPrefix DUSTTINY = new MaterialPrefix("powder_tiny", M/9, HBMMaterialIconType.dustTiny,  hasPowderProperty);
     public static final MaterialPrefix WIRE = new MaterialPrefix("wire", M/8, HBMMaterialIconType.wire, ntmMaterial -> ntmMaterial.hasFlag(WIRE));
     public static final MaterialPrefix DENSEWIRE = new MaterialPrefix("dense_wire",  M, HBMMaterialIconType.denseWire, ntmMaterial -> ntmMaterial.hasFlag(DENSE_WIRE));
     public static final MaterialPrefix BILLET = new MaterialPrefix("billet", M/6, HBMMaterialIconType.billet, ntmMaterial -> ntmMaterial.materialFlags.hasFlag(BILLET));
-    public static final MaterialPrefix GEM = new MaterialPrefix("gem", M, HBMMaterialIconType.gem, ntmMaterial -> ntmMaterial.hasFlag(GEM));
-    public static final MaterialPrefix CRYSTAL = new MaterialPrefix("crystal", null, INGOT.quantity, "crystal");
-    public static final MaterialPrefix DUST = new MaterialPrefix("powder", null, INGOT.quantity, "dust");
-    public static final MaterialPrefix PLATE = new MaterialPrefix("plate", null, INGOT.quantity, "plate");
-    public static final MaterialPrefix CASTPLATE = new MaterialPrefix("cast_plate", null, INGOT.quantity * 3, "plateTriple");
-    public static final MaterialPrefix WELDEDPLATE = new MaterialPrefix("welded_plate", null, INGOT.quantity * 6, "plateSextuple");
-    public static final MaterialPrefix QUART = new MaterialPrefix("quart", null, 162, "quart");
-    public static final MaterialPrefix SHELL = new MaterialPrefix("shell", null, INGOT.quantity * 4, "shell");
-    public static final MaterialPrefix PIPE = new MaterialPrefix("pipe", null, INGOT.quantity * 3, "ntmpipe");
-    public static final MaterialPrefix BLOCK = new MaterialPrefix("block", null, INGOT.quantity * 9, "block");
-    public static final MaterialPrefix HEAVY_COMPONENT = new MaterialPrefix( "heavy_component", null, CASTPLATE.quantity * 256, "componentHeavy");
+    public static final MaterialPrefix GEM = new MaterialPrefix("gem", M, HBMMaterialIconType.gem, hasGemProperty);
+    public static final MaterialPrefix CRYSTAL = new MaterialPrefix("crystal", 2*M, HBMMaterialIconType.crystal, hasOreProperty); //TODO: implement has ore for this one
+    public static final MaterialPrefix DUST = new MaterialPrefix("powder", M, HBMMaterialIconType.powder, hasPowderProperty);
+    public static final MaterialPrefix PLATE = new MaterialPrefix("plate", M, HBMMaterialIconType.plate, hasIngotProperty.and(ntmMaterial -> ntmMaterial.hasFlag(PLATE)));
+    public static final MaterialPrefix CASTPLATE = new MaterialPrefix("cast_plate", 3*M, HBMMaterialIconType.castPlate, hasIngotProperty.and(ntmMaterial -> ntmMaterial.materialFlags.hasFlag(CAST_PLATE)));
+    public static final MaterialPrefix WELDEDPLATE = new MaterialPrefix("welded_plate", M*6, HBMMaterialIconType.weldedPlate, hasIngotProperty.and(ntmMaterial -> ntmMaterial.hasFlag(WELDED_PLATE)));
+    public static final MaterialPrefix SHELL = new MaterialPrefix("shell", M*4, HBMMaterialIconType.shell, hasIngotProperty.and(ntmMaterial -> ntmMaterial.hasFlag(SHELL)));
+    public static final MaterialPrefix PIPE = new MaterialPrefix("ntmpipe", M*3, HBMMaterialIconType.pipe, hasIngotProperty.and(ntmMaterial -> ntmMaterial.hasFlag(PIPE)));
+    public static final MaterialPrefix BLOCK = new MaterialPrefix("block", M*9, HBMMaterialIconType.block, hasIngotProperty.or(ntmMaterial -> ntmMaterial.hasFlag(BLOCK)));
+    public static final MaterialPrefix HEAVY_COMPONENT = new MaterialPrefix("heavy_component", M*(256*3), HBMMaterialIconType.heavyComponent, hasIngotProperty.and(ntmMaterial -> ntmMaterial.hasFlag(CASTPLATE)));
+    public static final MaterialPrefix BOLT = new MaterialPrefix("bolt", M/8, HBMMaterialIconType.bolt, hasIngotProperty.and(ntmMaterial -> ntmMaterial.hasFlag(BOLT)));
 
-    public static final MaterialPrefix BOLT = new MaterialPrefix("bolt", null, 9);
+//    public static final MaterialPrefix QUANTUM = new MaterialPrefix("quantum", M/72, HBMMaterialIconType.quantum, Predicates.alwaysTrue(), null); // 1/72 of an ingot, allows the ingot to be divisible through 2, 4, 6, 8, 9, 12, 24 and 36 (idk why we need it)
+//    public static final MaterialPrefix QUART = new MaterialPrefix("quart", null, 162, "quart"); cant find it in game?
 
-
-    public static final MaterialPrefix LIGHTBARREL = new MaterialPrefix("light_barrel", null, INGOT.quantity * 3, "barrelLight");
-    public static final MaterialPrefix HEAVYBARREL = new MaterialPrefix("heavy_barrel", null, INGOT.quantity * 6, "barrelHeavy");
-    public static final MaterialPrefix LIGHTRECEIVER = new MaterialPrefix("LIGHT_RECEIVER", null, INGOT.quantity * 4, "receiverLight");
-    public static final MaterialPrefix HEAVYRECEIVER = new MaterialPrefix("HEAVYRECEIVER", null, INGOT.quantity * 9, "receiverHeavy");
-    public static final MaterialPrefix MECHANISM = new MaterialPrefix("MECHANISM", null, INGOT.quantity * 4, "gunMechanism");
-    public static final MaterialPrefix STOCK = new MaterialPrefix("STOCK", null, INGOT.quantity * 4, "stock");
-    public static final MaterialPrefix GRIP = new MaterialPrefix("GRIP", null, INGOT.quantity * 2, "grip");
+    //TODO: when HBM gun port happens
+//    public static final MaterialPrefix LIGHTBARREL = new MaterialPrefix("light_barrel", null, INGOT.quantity * 3, "barrelLight");
+//    public static final MaterialPrefix HEAVYBARREL = new MaterialPrefix("heavy_barrel", null, INGOT.quantity * 6, "barrelHeavy");
+//    public static final MaterialPrefix LIGHTRECEIVER = new MaterialPrefix("LIGHT_RECEIVER", null, INGOT.quantity * 4, "receiverLight");
+//    public static final MaterialPrefix HEAVYRECEIVER = new MaterialPrefix("HEAVYRECEIVER", null, INGOT.quantity * 9, "receiverHeavy");
+//    public static final MaterialPrefix MECHANISM = new MaterialPrefix("MECHANISM", null, INGOT.quantity * 4, "gunMechanism");
+//    public static final MaterialPrefix STOCK = new MaterialPrefix("STOCK", null, INGOT.quantity * 4, "stock");
+//    public static final MaterialPrefix GRIP = new MaterialPrefix("GRIP", null, INGOT.quantity * 2, "grip");
 
 
     private final Set<NTMMaterial> generatedMaterials = new HashSet<>();
